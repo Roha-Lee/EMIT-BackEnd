ef7b2d6155afd7f31b043f7dc6e9308c76af4dde
chauneng <50446006+chauneng@users.noreply.github.com>
2022-02-19 21:36:56 +0900

    feature: daily statistics

--- practice/routes/statistics.js
+++ practice/routes/statistics.js
@@ -7,11 +7,59 @@ const dbconfig = require('../config/database');
 const router = express.Router();
 const connection = mysql.createConnection(dbconfig);
 
-router.get('/statistics', verifyToken, (req, res) => {
-  // console.log(req.decoded, 1);
-  // console.log(req.body, 1);
-  const { userInfo } = req.decoded;
+router.get('/daily', verifyToken, (req, res) => {
+  // const { userInfo } = req.decoded;
   const { today } = req.body;
+  try {
+    const sql = `SELECT study_durations.subject_id, 
+    study_durations.start_time, 
+    study_durations.updated_at, 
+    subjects.name,
+    colors.code 
+    FROM study_durations 
+    JOIN subjects 
+    ON study_durations.subject_id = subjects.id
+    JOIN colors 
+    ON colors.id = subjects.color_code_id 
+    WHERE study_durations.start_time >= STR_TO_DATE("${today}", "%Y-%m-%d") 
+    AND study_durations.updated_at < DATE_ADD(STR_TO_DATE("${today}", "%Y-%m-%d"), INTERVAL 1 day);`;
+    connection.query(sql, (err, row) => {
+      if (err) throw err;
+      const studyTimeSummary = row.map((item) => {
+        const start = new Date(`${item.start_time} GMT+0900`);
+        const end = new Date(`${item.updated_at} GMT+0900`);
+        return {
+          subjectId: item.subject_id,
+          subjectName: item.name,
+          color: item.code,
+          totalTime: end - start,
+        };
+      })
+        .reduce((prev, curr) => {
+          const arr = [...prev];
+          const idx = prev.findIndex((elem) => elem.subjectId === curr.subjectId);
+          if (idx === -1) {
+            arr.push({
+              subjectId: curr.subjectId,
+              subjectName: curr.subjectName,
+              color: curr.color,
+              totalTime: curr.totalTime,
+            });
+          } else {
+            arr[idx].totalTime += curr.totalTime;
+          }
+          return arr;
+        }, []);
+      return res.json({ studyTimeSummary });
+    });
+  } catch (error) {
+    return res.json({ error });
+  }
+});
+
+router.get('/period', verifyToken, (req, res) => {
+  const { userInfo } = req.decoded;
+  const { startTime, endTime } = req.body;
   try {
     const sql = `SELECT study_durations.subject_id, 
     study_durations.start_time, 
@@ -22,8 +70,9 @@ router.get('/statistics', verifyToken, (req, res) => {
     JOIN subjects
     JOIN colors
     ON subjects.user_id = "${userInfo.id}"
-    WHERE study_duration.start_time > ${today}
-    AND study_duration.update_at < ${today};`;
+    WHERE study_duration.start_time 
+    BETWEEN STR_TO_DATE("${startTime}", "%Y-%m-%d")
+    AND  STR_TO_DATE("${endTime}", "%Y-%m-%d");`;
     connection.query(sql, (err, row) => {
       if (err) throw err;
       // console.log(row);
@@ -60,9 +109,11 @@ router.get('/statistics', verifyToken, (req, res) => {
           }
           return next();
         }, []);
-      return res.json({ databaseRows, studyTimeSummary });
+      return res.json({ studyTimeSummary });
     });
   } catch (error) {
     return res.json({ error });
   }
 });
+
+module.exports = router;
