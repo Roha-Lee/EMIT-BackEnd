3df453e30fedf2c3da3a76ab81e28d7b599e4670
chauneng <50446006+chauneng@users.noreply.github.com>
2022-02-22 22:01:17 +0900

    feature: todo del/mod

--- practice/routes/todos.js
+++ practice/routes/todos.js
@@ -8,15 +8,17 @@ const router = express.Router();
 
 // router.post('/', verifyToken, (req, res) => {
 router.post('/', (req, res) => {
-  console.log(req.decoded, 1);
-  console.log(req.body, 1);
+  // console.log(req.decoded, 1);
+  // console.log(req.body, 1);
   const { content, subjectId } = req.body;
   // const { userInfo } = req.decoded;
-  const userid = 1;
-  console.log(typeof parseInt(subjectId, 10));
+  // console.log(typeof parseInt(subjectId, 10));
+  if (content?.length === 0 || content === undefined) {
+    return res.json({ error: 'empty content field' });
+  }
   try {
+    const insertsql = `INSERT INTO todos(content, subject_id, user_id) VALUES("${content}", ${parseInt(subjectId, 10)}, ${parseInt(1, 10)})`;
     // const insertsql = `INSERT INTO todos(content, subject_id, user_id) VALUES("${content}", ${parseInt(subjectId, 10)}, ${userInfo.id})`;
-    const insertsql = `INSERT INTO todos(content, subject_id, user_id) VALUES("${content}", ${parseInt(subjectId, 10)}, ${userid})`;
     connection.query(insertsql, (insertErr, result, fields) => {
       console.log(result);
       if (insertErr) throw insertErr;
@@ -24,10 +26,10 @@ router.post('/', (req, res) => {
         todo: {
           id: result.insertId,
           content,
-          subjectId: parseInt(subjectId, 10),
+          subject_id: parseInt(subjectId, 10),
           // userId: userInfo.id,
-          userId: 1,
-          isDone: false,
+          user_id: 1,
+          is_done: false,
         },
       });
     });
@@ -36,43 +38,25 @@ router.post('/', (req, res) => {
   }
 });
 
-router.delete('/delete', (req, res) => {
-  const userId = 1;
-  // const { userId } = req.decoded;
-  const { todoId } = req.body;
-  const sql = `DELETE FROM todos 
-  WHERE todos.id = "${todoId}"
-  AND todos.user_id = ${userId}`;
-
-
-});
-
 // todo 이미 있는 내용은 추가로 생성하지 않게 해야할까? 같은 것이 들어가면 안되는 이유도 없으니 그냥 유저가 알아서 지우게 하는게 나을 듯
 
-router.patch('/:todoId', verifyToken, (req, res) => {
+router.patch('/:todoID', verifyToken, (req, res) => {
   const { content, isDone } = req.body;
   const { userInfo } = req.decoded;
-  const { todoId } = parseInt(req.params, 10);
+  const { todoID } = req.params;
+  // console.log(todoId)
   try {
-    const findsql = `SELECT * FROM todos WHERE id = "${todoId}";`;
+    const findsql = `SELECT * FROM todos WHERE id = "${parseInt(todoID, 10)}";`;
     connection.query(findsql, (finderr, row) => {
-      if (row.lenth === 0 || userInfo.id === row.shift().id) {
+      if (row.lenth === 0 || userInfo.id !== row.shift().user_id) {
         res.json({ message: 'Invalid user' });
       } else {
-        const oldTodo = row.shift();
-        const updatesql = `UPDATE todos SET content = "${content}", isDone = "${isDone}") WHERE id = ${todoId})`;
+        // const oldTodo = row.shift();
+        const updatesql = `UPDATE todos SET content = "${content}", is_done = ${parseInt(isDone, 10)} WHERE user_id = ${parseInt(userInfo.id, 10)} AND id = ${parseInt(todoID, 10)}`;
         try {
           connection.query(updatesql, (updateErr, result, fields) => {
             if (updateErr) throw updateErr;
-            res.json({
-              todo: {
-                id: result.insertId,
-                content,
-                subjectId: oldTodo.subjectId,
-                userId: userInfo.id,
-                isDone: false,
-              },
-            });
+            res.json({ message: 'success' });
           });
         } catch (error) {
           return res.json({ message: error });
@@ -84,39 +68,14 @@ router.patch('/:todoId', verifyToken, (req, res) => {
   }
 });
 
-router.patch('/:todoId', verifyToken, (req, res) => {
-  const { content, isDone } = req.body;
-  const { userInfo } = req.decoded;
-  const { todoId } = parseInt(req.params, 10);
-  try {
-    const findsql = `SELECT * FROM todos WHERE id = "${todoId}";`;
-    connection.query(findsql, (finderr, row) => {
-      if (row.lenth === 0) {
-        res.json({ message: 'Invalid request' });
-      } else {
-        const oldTodo = row.shift();
-        const updatesql = `UPDATE todos SET content = "${content}", isDone = "${isDone}") WHERE id = ${todoId})`;
-        try {
-          connection.query(updatesql, (updateErr, result, fields) => {
-            if (updateErr) throw updateErr;
-            res.json({
-              todo: {
-                id: result.insertId,
-                content,
-                subjectId: oldTodo.subjectId,
-                userId: userInfo.id,
-                isDone: false,
-              },
-            });
-          });
-        } catch (error) {
-          return res.json({ message: error });
-        }
-      }
-    });
-  } catch (err) {
-    return res.json({ message: 'Failed to reach data', err });
-  }
+router.delete('/:todoID', (req, res) => {
+  //const user_id = // 쿼리 수정
+  const { todoID } = req.params;
+  connection.query(`DELETE FROM todos WHERE id = ${todoID};`, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    return res.status(200).send({ message: 'success' });
+  });
 });
 
 module.exports = router;
